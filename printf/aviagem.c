898bca9bc4f312b708467b4d7d4f62089cacbdb2
agarf <fotografofraga@gmail.com>
2025-05-31 23:04:28 -0300

    mais um codigo - aviagem.

---
+++ printf/aviagem.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+
+int main(void) {
+    printf("Estamos reassistindo a viagem.\n");
+    printf("Neste ano de 2025.\n");
+    printf("E so a novela parar para a zulma chorar.\n");
+}
